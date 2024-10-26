272e877328ba4bbc98debed6d0e157592d5a6fe7
Asad Ali Panhwar <asadalipuh5@gmail.com>
2024-10-26 22:38:26 +0500

    requirements.txt installed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'detection',
+    version= '0.0.0',
+    author= 'Asad Ali Panhwar',
+    author_email= 'asadalipuh5@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
